d41f7b6bfde0a1d670d985bd3d0641d00932af59
dkozlikina <kozlikina@sfedu.ru>
2023-10-28 22:24:13 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
+using System.Security.Cryptography;
+using System.Threading;
+using static System.Windows.Forms.LinkLabel;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TreeView;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap map;
+
+        class point
+        {
+            int x;
+            int y;
+            Color color;
+
+            point(int x, int y, Color color)
+            {
+                this.x = x;
+                this.y = y;
+                this.color = color;
+            }
+
+            //void draw()
+            //{
+            //    for (int x = this.x - 3; x < this.x + 3; x++)
+            //    {
+            //        for (int y = this.y - 3; y < this.y + 3; y++)
+            //        {
+            //            map.SetPixel(x, y, this.color);
+            //        }
+            //    }
+            //}
+        }
+
+        class edge
+        {
+            point a;
+            point b;
+            Color color;
+
+            edge(point a, point b, Color color)
+            {
+                this.a = a;
+                this.b = b;
+                this.color = color;
+            }
+        }
+
+        class face
+        {
+            List<edge> edges;
+            face(List<edge> edges)
+            {
+                this.edges = edges;
+            }
+        }
+
+        class polyhedron
+        {
+            List<face> faces;
+            polyhedron(List<face> faces)
+            {
+                this.faces = faces;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
